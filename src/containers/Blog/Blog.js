b319d6a725293b8510765691c5929214cd82530d
Chris Stromquist <chris.stromquist@gmail.com>
2018-06-08 14:59:32 -0700

    Section 11, Lecture 200 - Implementing guards (auth)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,17 +7,21 @@ import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
+	state = {
+		auth: false
+	}
+
 	render() {
 		return (
 			<div className="Blog">
 				<header>
 					<nav>
 						<ul>
-							<li><NavLink 
-								to="/posts" 
+							<li><NavLink
+								to="/posts"
 								exact
 								activeClassName="my-active"
-								activeStyle={{fontWeight: 'bold', textDecoration: 'underline'}}>Posts</NavLink></li>
+								activeStyle={{ fontWeight: 'bold', textDecoration: 'underline' }}>Posts</NavLink></li>
 							<li><NavLink to={{
 								pathname: '/new-post',
 								hash: '#submit',
@@ -29,7 +33,7 @@ class Blog extends Component {
 				{/* <Route path="/" exact render={() => <h1>Home</h1>} />
 				<Route path="/" render={() => <h1>Home 2</h1>} /> */}
 				<Switch>
-					<Route path="/new-post" component={NewPost} />
+					{this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
 					<Route path="/posts" component={Posts} />
 					<Redirect from="/" to="/posts" />
 					{/* <Route path="/" component={Posts} /> */}
