4469f31098c0f4b2c033b6d9a428c56b6f1b5f49
Chris Stromquist <chris.stromquist@gmail.com>
2018-06-08 11:49:18 -0700

    Section 11, Lecture 177 - Added links

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios';
+import axios from './axios';
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
